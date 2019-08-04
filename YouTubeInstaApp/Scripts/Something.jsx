ba1713b98cce903968be3952998052496af06899
Alexandr <saint12maloj@gmail.com>
2019-08-04 17:30:01 +0300

    WIP: SPA (all components - from one page), APIs (IFrame instead)

--- YouTubeInstaApp/Scripts/Something.jsx
+++
@@ -1,9 +0,0 @@
-﻿class SomethingClass extends React.Component {
-    render() {
-        return <h1>Hi, I am SOMETHING!</h1>;
-    }
-}
-ReactDOM.render(
-    <SomethingClass />,
-    document.getElementById("somethingContent")
-);
